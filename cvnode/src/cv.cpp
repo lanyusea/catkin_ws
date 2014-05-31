7617bf6b9e9707b18a3770b480cbd5e003cfdecc
aqua <lanyusea@gmail.com>
2014-05-31 23:14:47 +0300

    edge detection

--- cvnode/src/cv.cpp
+++ cvnode/src/cv.cpp
@@ -4,8 +4,14 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace std;
+using namespace cv;
 
 static const std::string OPENCV_WINDOW = "Image window";
+static const std::string EDGE_WINDOW= "Edge Map";
 
 class ImageConverter
 {
@@ -13,9 +19,6 @@ class ImageConverter
     image_transport::ImageTransport it_;
     image_transport::Subscriber image_sub_;
     image_transport::Publisher image_pub_;
-  //compressed_image_transport::ImageTransport it_;
-  //compressed_image_transport::Subscriber image_sub_;
-  //compressed_image_transport::Publisher image_pub_;
 
 public:
   ImageConverter()
@@ -27,20 +30,32 @@ public:
     image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
     cv::namedWindow(OPENCV_WINDOW);
+    cv::namedWindow(EDGE_WINDOW);
   }
 
   ~ImageConverter()
   {
     cv::destroyWindow(OPENCV_WINDOW);
+    cv::destroyWindow(EDGE_WINDOW);
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
     cv_bridge::CvImagePtr cv_ptr;
+    cv::Mat dst,detected_edges;
+    cv::Mat src,src_gray;
+    int edgeThresh = 1;
+    int lowThreshold;
+    int const max_lowThreshold = 100;
+    int ratio = 3;
+    int kernel_size = 3;
+
     try
     {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      src = cv_ptr->image;
     }
+
     catch (cv_bridge::Exception& e)
     {
       ROS_ERROR("cv_bridge exception: %s", e.what());
@@ -48,22 +63,31 @@ public:
     }
 
     // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+    //if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+    //      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
 
     // Update GUI Window
     cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
 
     // Output modified video stream
     image_pub_.publish(cv_ptr->toImageMsg());
-  }
+
+    dst.create(src.size(),src.type());
+    cvtColor(src,src_gray,CV_BGR2GRAY);
+    blur( src_gray, detected_edges, Size(3,3) );
+    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+    dst = Scalar::all(0);
+    src.copyTo( dst, detected_edges);
+    imshow( EDGE_WINDOW, dst );
+
+    cv::waitKey(3);
+    }
 };
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+ros::init(argc, argv, "image_converter");
+ImageConverter ic;
+ros::spin();
+return 0;
 }
