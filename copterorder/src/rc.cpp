60e84aac6d2b9b990b790d28edf9d540506abd5b
aqua <lanyusea@gmail.com>
2014-06-01 19:47:57 +0300

    controleer add

---
+++ copterorder/src/rc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ros/ros.h>
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    ros::init(argc,argv, "rf");
+    ros::NodeHandler nh_;
+    ros::Publisher pub_;
+
+
+    return 0;
+}
