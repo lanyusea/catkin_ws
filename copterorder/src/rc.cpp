35009b59be84b4dd51b6bb79c7b359e2c248326e
aqua <lanyusea@gmail.com>
2014-06-04 00:05:28 +0300

    TODO

--- copterorder/src/rc.cpp
+++ copterorder/src/rc.cpp
@@ -5,9 +5,20 @@ using namespace std;
 int main(int argc, char ** argv)
 {
     ros::init(argc,argv, "rf");
-    ros::NodeHandler nh_;
+    ros::NodeHandle nh_;
     ros::Publisher pub_;
 
+//TODO:
+//ros::service::call /command for mode change
+//
 
+//TODO:
+//ros::service::call /waypoint to set destination
+//7param:https://github.com/epsilonorion/roscopter/wiki/Exploring-Waypoint-Control
+
+//TODO:
+//publish to /send_rc node to rewrite RC channel value
+//topic /rc is published by mavlink for current rc value
+//
     return 0;
 }
