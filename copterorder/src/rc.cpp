9c94b1e2b16feb7584310e4f9a11ddf54ed1771a
aqua <lanyusea@gmail.com>
2014-07-27 00:22:50 +0300

    armed in auto mode

--- copterorder/src/rc.cpp
+++ copterorder/src/rc.cpp
@@ -6,86 +6,24 @@
 #include "roscopter/XBEECommand.h"
 using namespace std;
 
-class CMD {
-    public:
-        CMD() {
-            mode = 12;
-        }
-        int getMode() {
-            return mode;
-        }
-        void changeCMD(int newCMD) {
-            if ((newCMD>=0)&&(newCMD<=12)){
-                mode = newCMD;
-                cout << "cmd updated" << endl;
-            }
-            else
-                cout << "invalid cmd" <<endl;
-        }
-        string getName(){
-            switch (mode){
-                case 1:
-                    return "LAUNCH";break;
-                case 2:
-                    return "LAND";break;
-                case 3:
-                    return "ARM";break;
-                case 4:
-                    return "DISARM";break;
-                case 5:
-                    return "SET MANUAL";break;
-                case 6:
-                    return "SET HOLD";break;
-                case 7:
-                    return "SET STABILIZE";break;
-                case 8:
-                    return "SET ALT_HOLD";break;
-                case 9:
-                    return "SET AUTO"; break;
-                case 10:
-                    return "SET LOITER"; break;
-                case 11:
-                    return "SET LAND";break;
-                case 12:
-                    return "RC control";break;
-            }
-        }
-    private:
-        int mode;
-
-};
-
-
-void publishCMD(ros::ServiceClient client, CMD cmd) {
-    roscopter::APMCommand srv;
-    srv.request.command = cmd.getMode();
-    if (client.call(srv)) {
-        ROS_INFO("Publish successful, mode: %s", cmd.getName().c_str());
-    }
-}
 
 int main(int argc, char ** argv)
 {
     ros::init(argc,argv, "rf");
     ros::NodeHandle nh;
-    ros::ServiceClient commandClient = nh.serviceClient<roscopter::APMCommand>("command");
-    CMD cmd;
 
-    //ros::Publisher waypointPub = nh.advertise<roscopter::SendWaypoint>()
-    ros::ServiceClient waypointPub = nh.serviceClient<roscopter::SendWaypoint>("waypoint");
+    ros::ServiceClient apm_cmd_ = nh.serviceClient<roscopter::APMCommand>("command");
 
+    roscopter::APMCommand srv;
+    ROS_INFO("Trying to arm");
+    if (apm_cmd_.call(srv))
+        ROS_WARN("Arm request complete");
+    else
+        ROS_ERROR("Arm request incomplete");
 
+    roscopter::SendWaypoint wayptListPub = rospy.Publisher('/waypoint_list', roscopter.msg.WaypointList);
+    roscopter::SendWaypoint wayptListMsg = roscopter.msg.WaypointList();
 
-//TODO:
-//ros::service::call /command for mode change
-//
-
-//TODO:
-//7param:https://github.com/epsilonorion/roscopter/wiki/Exploring-Waypoint-Control
-
-//TODO:
-//publish to /send_rc node to rewrite RC channel value
-//topic /rc is published by mavlink for current rc value
-//
-    return 0;
 }
+
+
--- roscopter/scripts/waypoint_tester.py
+++ roscopter/scripts/waypoint_tester.py
@@ -1,40 +1,54 @@
 #!/usr/bin/env python
 import rospy
 import roscopter.msg
+import roscopter.srv
 
 def publish_waypt():
     rospy.init_node('waypoint_tester')
 
     wayptListPub = rospy.Publisher('/waypoint_list', roscopter.msg.WaypointList)
 
-    while not rospy.is_shutdown():
-        wayptListMsg = roscopter.msg.WaypointList()
+    wayptListMsg = roscopter.msg.WaypointList()
 
-        # Populate waypoint list message
-        for i in range (0,20):
-            wayptMsg = roscopter.msg.Waypoint()
+    # Populate waypoint list message
+    for i in range (0,2):
+        wayptMsg = roscopter.msg.Waypoint()
 
-            wayptMsg.latitude = (29.662161 + i) * 1E7
-            wayptMsg.longitude = (-82.3777551 + i) * 1E7
-            wayptMsg.altitude = 100 + i;
-            wayptMsg.pos_acc = (10 + i) * 1000;
-            wayptMsg.speed_to = 20 + i;
-            wayptMsg.altitude = (30 + i) * 1000;
-            wayptMsg.hold_time = (40 + i) * 1000;        # Assume defined in seconds
-            wayptMsg.yaw_from = (50 + i) * 1000;
+        wayptMsg.latitude = (29.662161 + i) * 1E7
+        wayptMsg.longitude = (-82.3777551 + i) * 1E7
+        wayptMsg.altitude = 100 + i;
+        wayptMsg.pos_acc = (10 + i) * 1000;
+        wayptMsg.speed_to = 20 + i;
+        wayptMsg.altitude = (30 + i) * 1000;
+        wayptMsg.hold_time = (40 + i) * 1000;        # Assume defined in seconds
+        wayptMsg.yaw_from = (50 + i) * 1000;
 
-            wayptMsg.waypoint_type = roscopter.msg.Waypoint.TYPE_NAV
+        wayptMsg.waypoint_type = roscopter.msg.Waypoint.TYPE_NAV
 
-            wayptListMsg.waypoints.append(wayptMsg)
+        wayptListMsg.waypoints.append(wayptMsg)
 
+    rospy.sleep(1.0)
+    wayptListPub.publish(wayptListMsg)
+
+    print("Waypoints Sent")
+    rospy.sleep(2.0)
+
+def arm():
+    armsrv = rospy.ServiceProxy("command",roscopter.srv.APMCommand)
+    #roscopter.srv.APMCommand(3)
+    try:
+        result=armsrv(9)
+        print ("auto mode")
         rospy.sleep(1.0)
-        wayptListPub.publish(wayptListMsg)
-
-        print("Waypoints Sent")
-        rospy.sleep(10.0)
+        result2=armsrv(3)
+        print ("armed")
+    except rospy.ServiceException as exc:
+        print (str(exc))
 
 if __name__ == '__main__':
     try:
         publish_waypt()
+        arm()
+
     except  rospy.ROSInterruptException:
         pass
