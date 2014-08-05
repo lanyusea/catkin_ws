855b0190087d055d40166cb85c189f9f1bfb0163
aqua <lanyusea@gmail.com>
2014-08-05 22:00:17 +0800

    serial added

---
+++ serial_control/src/main.cpp
@@ -0,0 +1,12 @@
+#include <ros/ros.h>
+#include <SerialStream.h>
+#include <BoundingBox.h>
+#include <iostream>
+using namespace LibSerial;
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+
+    return 0;
+}
