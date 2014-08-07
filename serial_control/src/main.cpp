2299d567df2c60873822d4fdd64ffc346e2a0a09
aqua <lanyusea@gmail.com>
2014-08-07 21:17:27 +0800

    callback bug fix(use void)

--- serial_control/src/main.cpp
+++ serial_control/src/main.cpp
@@ -1,12 +1,12 @@
 #include <ros/ros.h>
 #include <SerialStream.h>
 #include <tld_msgs/BoundingBox.h>
-//#include <BoundingBox.h>
+#include <BoundingBox.h>
 #include <iostream>
 using namespace LibSerial;
 using namespace std;
 
-int callback(const tld_msgs::BoundingBox &data) {
+void callback(const tld_msgs::BoundingBox &data) {
     ROS_INFO("I heared the msg");
 }
 
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
     ros::NodeHandle n;
     tld_msgs::BoundingBox newMsg;
 
-    ros::Subscriber sub = n.subscribe("/tld_tracked_object", 20, callback);
+    ros::Subscriber sub = n.subscribe("/tld_tracked_object", 20, &callback);
 
 
 
